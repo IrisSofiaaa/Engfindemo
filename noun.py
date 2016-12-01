5f15464ab757a898083f07a03eb1bacb2434f245
Iris Knuntinen <lin503_iris@mumin.ling.su.se>
2016-12-01 22:21:16 +0100

    updated noun

--- noun.py
+++ noun.py
@@ -1,78 +1,49 @@
-print("NOUN")
-noun = d.dictionary.get(i)
-try:
-    trans = noun[2]
-except IndexError:
-    trans = noun[0]
-except TypeError:
-    trans = ""
-if i.endswith("s"):
-    trans = trans + "t"
-print(trans)
-mode = rest[i]
-print(i, mode)
+from case import Case
 
-strip1 = strip[0]
-strip2 = strip[1]
-strip3 = strip[2]
-strip4 = strip[3]
-strip5 = strip[4]
-strip6 = strip[5]
+class Noun(Case):
+    def __init__(self, rest, d, c, i):
+        self.rest = rest
+        self.d = d
+        self.trans = ""
+        self.case = c.case
+        self.i = i
+        
+    def noun(self):
+                noun = self.d.dictionary.get(self.i)
+                
+                try:
+                    self.trans = noun[2]
+                except IndexError:
+                    self.trans = noun[0]
+                except TypeError:
+                    self.trans = "-"
 
-print(strip1, strip2, strip3, strip4, strip5, strip6)
-sentence = " ".join([strip1, strip2, strip3, strip4, strip5, strip6])
-for i in strip:
-    t = d.dictionary.get(i)
-    try:
-        print(t[2])
-    except IndexError:
-        print(t[0])
-    except TypeError:
-
-        print("")
-
-for i in sentence.split():
-
-    if i == strip1:
-        prev = "None"
-    elif i == strip2:
-        prev = strip1
-    elif i == strip3:
-        prev = strip2
-    elif i == strip4:
-        prev = strip3
-    elif i == strip5:
-        prev = strip4
-    else:
-        prev = strip5
-
-    return prev
-if i == "hers" or i == "his":
-    trans = "hänen"
-    print(trans)
-elif i == "my" or i == "mine":
-    trans = "minun"
-    print(trans)
-elif i == "theirs":
-    trans = "heidän"
-    print(trans)
-elif i == "our":
-    trans = "meidän"
-    print(trans)
-elif i == "yours":
-    trans = "sinun"
-    print(trans)
-if prev == "to" or prev == "into":
-    trans = trans + "een"
-    print(trans)
-elif prev == "in":
-    trans = trans + "ssa"
-    print(trans)
-elif prev == "for":
-    trans = trans + "lle"
-    print(trans)
-elif prev == "at":
-    trans = trans + "ssa"
-    print(trans)
-else:
-    pass
+                if self.rest[self.i] == "NNS":
+                    if self.trans.endswith("hti"):
+                        self.trans = self.trans[:2] + "det"
+                    elif self.trans.endswith("ti"):
+                        self.trans = self.trans[:-2] + "dit"
+                    elif self.trans.endswith("us"):
+                        self.trans = self.trans[:-1] + "kset"
+                    elif self.trans.endswith("e"):
+                        self.trans = self.trans + "et"
+                    elif self.trans.endswith("kä"):
+                        self.trans = self.trans[:-2] + "gät"
+                    else:
+                        self.trans = self.trans + "t"
+                elif self.i == "Her" or self.i == "her":
+                    self.trans = "hänen"
+                elif self.i == "His" or self.i == "his":
+                    self.trans = "hänen"
+                elif self.i == "My" or self.i == "my":
+                    self.trans = "minun"
+                elif self.i == "Their" or self.i == "their":
+                    self.trans = "heidän"
+                elif self.i == "Our" or self.i == "our":
+                    self.trans = "meidän"
+                elif self.i == "Your" or self.i == "your":
+                    self.trans = "sinun"
+                else:
+                    self.trans = self.trans + self.case
+                    
+   
